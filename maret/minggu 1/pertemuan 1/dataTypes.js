7cf438869acbf6190c5261fda81833a7cc99e73a
Fahmi Azzuhri Efki <91070416+fahmiazz@users.noreply.github.com>
2022-03-06 02:07:30 +0700

    Add files via upload

---
+++ maret/minggu 1/pertemuan 1/dataTypes.js
@@ -0,0 +1,11 @@
+var string = "ini string ya gais";
+var integer = 3; //integer angka bilangan bulat
+var float = 9.0; //float angka bilangan desimal
+var array = [array, string, float];
+var object = {
+  id: 1999,
+  car: "Honda Jazz",
+  year: 2020,
+};
+
+console.log(object);
---
+++ maret/minggu 1/pertemuan 1/function.js
@@ -0,0 +1,11 @@
+function getName(name) {
+  var age = getAge(23);
+  console.log(name);
+  console.log(age);
+}
+
+getName("Fahmi");
+
+function getAge(age) {
+  return age;
+}
